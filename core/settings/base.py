d3eabcef03ebdd1732b2e1935a6284ead37e4383
byagge <byagge@outlook.com>
2025-06-01 15:04:21 +0600

    Update base.py

--- core/settings/base.py
+++ core/settings/base.py
@@ -19,7 +19,9 @@ BASE_DIR = os.path.dirname(PROJECT_DIR)
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
-
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY')
+DEBUG = os.environ.get('DJANGO_DEBUG') == 'True'
+ALLOWED_HOSTS = os.environ.get('DJANGO_ALLOWED_HOSTS', '').split(',')
 
 # Application definition
 
